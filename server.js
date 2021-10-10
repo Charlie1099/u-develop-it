4fa7706a6f2e0ed8571d3a9022518d352ca42ba9
charlie1099 <cdcharlesworth@gmail.com>
2021-10-10 17:34:56 -0600

    added voter table

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utilis/inputCheck');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,13 +8,11 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/api", apiRoutes);
 
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
-
-
-
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
